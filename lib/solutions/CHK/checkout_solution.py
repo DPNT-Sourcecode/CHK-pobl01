50d38f59abf3a1ba773b49c4d96d7ba456012306
sbx_user1051 <sbx_user1051@169.254.89.185>
2023-06-08 12:55:17 +0000

    Thu Jun 08 12:55:17 UTC 2023

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -1,6 +1,10 @@
-
+import re
 
 # noinspection PyUnusedLocal
 # skus = unicode string
 def checkout(skus):
-    raise NotImplementedError()
+    item_a = len(list(re.finditer("A", skus)))
+    item_b = len(list(re.finditer("B", skus)))
+    item_c = len(list(re.finditer("C", skus)))
+    item_d = len(list(re.finditer("D", skus)))
+
