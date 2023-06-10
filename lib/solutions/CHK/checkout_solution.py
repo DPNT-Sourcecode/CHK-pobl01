7f4d2c995732b86898c18e18ad496feedcba2a62
sbx_user1051 <sbx_user1051@169.254.130.157>
2023-06-10 15:16:36 +0000

    Sat Jun 10 15:16:36 UTC 2023

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -21,32 +21,32 @@ def count_skus(skus):
 # skus = unicode string
 def checkout(skus):
 
-    sku_price_table = {"A": 50,
-                       "B": 30,
-                       "C": 20,
-                       "D": 15,
-                       "E": 40,
-                       "F": 10,
-                       "G": 20,
-                       "H": 10,
-                       "I": 35,
-                       "J": 60,
-                       "K": 80,
-                       "L": 90,
-                       "M": 15,
-                       "N": 40,
-                       "O": 10,
-                       "P": 50,
-                       "Q": 30,
-                       "R": 50,
-                       "S": 30,
-                       "T": 20,
-                       "U": 40,
-                       "V": 50,
-                       "W": 20,
-                       "X": 90,
-                       "Y": 10,
-                       "Z": 50}
+    sku_price_table = { "A": 50,
+                        "B": 30,
+                        "C": 20,
+                        "D": 15,
+                        "E": 40,
+                        "F": 10,
+                        "G": 20,
+                        "H": 10,
+                        "I": 35,
+                        "J": 60,
+                        "K": 70,
+                        "L": 90,
+                        "M": 15,
+                        "N": 40,
+                        "O": 10,
+                        "P": 50,
+                        "Q": 30,
+                        "R": 50,
+                        "S": 20,
+                        "T": 20,
+                        "U": 40,
+                        "V": 50,
+                        "W": 20,
+                        "X": 17,
+                        "Y": 20,
+                        "Z": 21}
 
     discount_thresholds = {"two": 2,
                            "three": 3,
@@ -180,4 +180,5 @@ def checkout(skus):
 
 out = checkout("STXYZ")
 print(out)
+
 
